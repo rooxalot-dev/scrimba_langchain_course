f6dfc46a5a5aac44cc54291a8df2bf2d64a3e9b5
Rodrigo Martins de Azevedo <rooxalot@gmail.com>
2024-03-17 18:54:37 -0300

    Upload embedded chunks into vectorstore

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,52 @@
 import { resolve } from 'path';
 import { readFile } from 'fs/promises';
 
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { config } from 'dotenv';
 
-const init = async () => {
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { SupabaseVectorStore } from '@langchain/community/vectorstores/supabase';
+import { OpenAIEmbeddings } from '@langchain/openai';
+
+import { createClient as createSupabaseClient } from "@supabase/supabase-js";
+
+const loadVectorstore = async () => {
   const filePath = resolve(__dirname, '..', 'resources', 'scrimba-info.txt');
 
   try {
     const resultText = await readFile(filePath, { encoding: 'utf8' });
-    console.log('Original Text', resultText.length);
 
     const chunkSize = 500;
     const chunkOverlapSize = chunkSize * 0.10;
-
     const textSplitter = new RecursiveCharacterTextSplitter({
       chunkSize: 500,
       chunkOverlap: chunkOverlapSize, // Configure how many characters from one chunk should appear in another (default: 200)
 
     });
     const documentsOutput = await textSplitter.createDocuments([resultText]);
-    console.log('Documents Text Output', documentsOutput.length);
+
+    const openAIApiKey = process.env.OPENAI_API_KEY || '';
+    const openAIEmbeddingsAlgorithm = new OpenAIEmbeddings({
+      openAIApiKey: openAIApiKey,
+    });
+
+    const supabaseUrl = process.env.SUPABASE_API_URL || '';
+    const supabaseKey = process.env.SUPABASE_KEY || '';
+    const supabaseClient = createSupabaseClient(supabaseUrl, supabaseKey);
+
+    await SupabaseVectorStore.fromDocuments(
+      documentsOutput,
+      openAIEmbeddingsAlgorithm,
+      {
+        client: supabaseClient,
+        tableName: 'documents',
+      }
+    );
+
+    console.log('Supabase vectorstore loaded!');
   } catch (error) {
     console.log('Error', error);
   }
 };
 
-init();
+config();
+loadVectorstore();
