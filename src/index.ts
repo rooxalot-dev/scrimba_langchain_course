07182f163a83346ad2150471ec9c1018807f1b73
Rodrigo Martins de Azevedo <rooxalot@gmail.com>
2024-03-17 17:57:15 -0300

    WIP: Read text info

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello World');
+import { readFile } from 'fs/promises';
+import { resolve } from 'path';
+
+const init = async () => {
+  const filePath = resolve(__dirname, '..', 'resources', 'scrimba-info.txt');
+
+  try {
+    const resultText = await readFile(filePath, { encoding: 'utf8' });
+    console.log('Original Text', resultText);
+  } catch (error) {
+    console.log('Error', error);
+  }
+};
+
+init();
