1c7a6fee904649523b89b9c4041bd2d70c7f8c9a
Rodrigo Martins de Azevedo <rooxalot@gmail.com>
2024-03-17 18:09:13 -0300

    WIP: Implement langchain's text splitter to generate chunks of info

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import { readFile } from 'fs/promises';
 import { resolve } from 'path';
+import { readFile } from 'fs/promises';
+
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 
 const init = async () => {
   const filePath = resolve(__dirname, '..', 'resources', 'scrimba-info.txt');
 
   try {
     const resultText = await readFile(filePath, { encoding: 'utf8' });
-    console.log('Original Text', resultText);
+    console.log('Original Text', resultText.length);
+
+    const textSplitter = new RecursiveCharacterTextSplitter();
+    const documentsOutput = await textSplitter.createDocuments([resultText]);
+    console.log('Documents Text Output', documentsOutput);
   } catch (error) {
     console.log('Error', error);
   }
