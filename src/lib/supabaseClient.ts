14fc8690cb210c188937293a75357acaec6694ab
Rodrigo Martins de Azevedo <rooxalot@gmail.com>
2024-03-19 21:54:59 -0300

    Fix lib methods not loading environment variables

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,4 +1,7 @@
+import { config } from 'dotenv';
 import { createClient as createSupabaseClient } from "@supabase/supabase-js";
+
+config();
 
 const supabaseUrl = process.env.SUPABASE_API_URL || '';
 const supabaseKey = process.env.SUPABASE_KEY || '';
--- src/lib/supabaseVectorStoreFactory.ts
+++ src/lib/supabaseVectorStoreFactory.ts
@@ -1,9 +1,12 @@
+import { config } from 'dotenv';
 import { OpenAIEmbeddings } from "@langchain/openai";
 import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
 
 import { supabaseClient } from "./supabaseClient";
 
 export const createSupabaseVectorStore = () => {
+  config();
+
   const openAIApiKey = process.env.OPENAI_API_KEY || '';
   const openAIEmbeddingsAlgorithm = new OpenAIEmbeddings({ openAIApiKey });
 
@@ -12,6 +15,7 @@ export const createSupabaseVectorStore = () => {
     {
       client: supabaseClient,
       tableName: 'documents', // When we first configure supabase with Langchain, we created a table called "documents"
+      queryName: 'match_documents', // When we first configure supabase with Langchain, we created a db function called "match_documents"
     }
   );
 
