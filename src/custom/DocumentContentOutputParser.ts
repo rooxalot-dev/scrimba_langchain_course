dcf11c5aea919317b5f59a6f19367af30830e5ef
Rodrigo <rooxalot@gmail.com>
2024-03-20 22:37:31 -0300

    Add custom document output parser

---
+++ src/custom/DocumentContentOutputParser.ts
@@ -0,0 +1,14 @@
+import {  } from '@langchain/core/output_parsers';
+import { DocumentInterface } from '@langchain/core/documents';
+
+export class DocumentContentOutputParser {
+
+  /**
+   *
+   * @param retrievedData List of DocumentInterfaces
+   * @returns list of parsed strings for each document
+   */
+  static parse(retrievedData: DocumentInterface<Record<string, any>>[]) {
+    return retrievedData.map((data => data.pageContent));
+  }
+}
